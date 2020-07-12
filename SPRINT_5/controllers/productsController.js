ab8cf7b5a951ae0c71501c2359c81e7eb2bec037
verosardin <vero.sardin28@gmail.com>
2020-07-12 14:02:22 -0300

    Cambié las funciones para que usando Sequelize y la base de datos, se pueda agregar, modificar y eliminar una tienda

--- SPRINT_5/controllers/productsController.js
+++ SPRINT_5/controllers/productsController.js
@@ -58,56 +58,25 @@ const productosController = {
 
         nuevoProducto: function (req,res){
 
-        let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-        let tiendas;
-        if(archivoTiendas == ""){
-           tiendas =[];
-        } else {
-            tiendas = JSON.parse(archivoTiendas);
-        }
-
-        nombreTiendas = [];
-            tiendas.forEach((tiend,i) => {
-                nombreTiendas.push(tiend.nombreTienda);
-            })
-        console.log(nombreTiendas);
-
-            res.render('productAdd', {tiendas: nombreTiendas});
+            db.Tienda.findAll()
+                .then (function(tiendas){
+                    res.render('productAdd', {tiendas: tiendas});
+                })
         },
-
-        crearProducto: function(req, res, next){
-
-            let producto = {
-                codigo: req.body.codigo,
-                nombreProducto: req.body.nombreProduct,
-                descCorta: req.body.descCorta,
-                descLarga: req.body.descLarga,
-                precio: req.body.precio,
-                tienda: req.body.tienda,
+            //FALTA
+        crearProducto: function(req, res){
+            db.Producto.create({
+                name: req.body.nombreProduct,
+                short_description: req.body.descCorta,
+                long_description: req.body.descLarga,
+                price: req.body.precio,
+                image: req.files[0].filename,
+                store_id: req.body.tienda,
                 filtroOrg: req.body.org,
                 filtroSinTacc: req.body.sinTacc,
-                filtrosSinLactosa: req.body.sinLactosa,
-                fotoProducto: req.files[0].filename
-            }
-            //GUARDAR INFORMACION DEL FORMULARIO EN UN ARCHIVO
-
-            //1ºLeer el archivo
-            let archivoProductos=fs.readFileSync('data_productos.json', {encoding: 'utf-8'});
-            let productos;
-            if(archivoProductos == ""){
-                productos =[];
-            } else {
-                productos = JSON.parse(archivoProductos);
-            }
-
-            //agregamos el usuarios Nuevo
-            productos.push(producto);
-            //Escribir el archivo
-            productosJSON = JSON.stringify(productos);
-
-            fs.writeFileSync('data_productos.json', productosJSON);
-
-            res.redirect('/productos');
+                filtrosSinLactosa: req.body.sinLactosa
+            })
+                res.redirect('/productos');
         },
 
         listadoProductos: function (req,res){
@@ -121,238 +90,138 @@ const productosController = {
 
         get_searchProductos: function (req,res){
             // con req.query.<dato> obtenemos el dato que viene del cliente en el cuerpo del form por GET
-            let nombreBuscado =  req.query.nombre;
-            let archivoProductos=fs.readFileSync('data_productos.json', {encoding: 'utf-8'});
-            let productos;
-            if(archivoProductos == ""){
-                productos =[];
-            } else {
-                productos = JSON.parse(archivoProductos);
-            }
 
-            let results = [];
-            productos.forEach((prod, i) => {
-                if (prod.nombreProducto.includes(nombreBuscado)) {
-                    results.push(prod);
-                }
-            });
-            return res.render('productoBuscado', {productos: results});
+            db.Producto.findAll({
+                include: ["tienda"],
+            })
+                .then(function(productos){
+
+                    let nombreBuscado =  req.query.nombre;
+                    let results = [];
+                    productos.forEach((prod, i) => {
+                        if (prod.name.includes(nombreBuscado)) {
+                            results.push(prod);
+                        }
+                    });
+                    return res.render('listadoProductos', {productos: results});
+                })
         },
 
+        //FALTA
         deleteProduct: function(req,res){
-            let archivoProductos=fs.readFileSync('data_productos.json', {encoding: 'utf-8'});
-            let productos;
-            if(archivoProductos == ""){
-                productos =[];
-            } else {
-                productos = JSON.parse(archivoProductos);
-            }
-
-            let producto = null;
-            productos.forEach((prod, i) => {
-                if (prod["codigo"] == req.params.codigo) {
-                    producto = prod;
-                }
-            });
-
-            let nuevoArrayProductos = [];
-            nuevoArrayProductos = productos.filter(prod => prod.codigo != producto.codigo);
-            nuevoArrayProductosJSON = JSON.stringify(nuevoArrayProductos);
-            fs.writeFileSync('data_productos.json', nuevoArrayProductosJSON);
-            return res.render("listadoProductos",{productos: nuevoArrayProductos});
+            console.log(req.params.codigo);
+            db.Producto.destroy({
+                where: {id: req.params.codigo}
+            })
+                .then(function(){
+                    res.redirect("productos");
+                })
         },
 
         get_editProducto: function(req,res){
-            let archivoProductos=fs.readFileSync('data_productos.json', {encoding: 'utf-8'});
-            let productos;
-            if(archivoProductos == ""){
-                productos =[];
-            } else {
-                productos = JSON.parse(archivoProductos);
-            }
+                let pedidoProducto = db.Producto.findByPk(req.params.codigo,{
+                    include: ["tienda"]
+                })
+                let pedidoTiendas = db.Tienda.findAll();
 
-            let producto = null;
-            productos.forEach((prod, i) => {
-                if (prod["codigo"] == req.params.codigo) {
-                    producto = prod;
-                }
-            });
-            let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-            let tiendas;
-            if(archivoTiendas == ""){
-                tiendas =[];
-            } else {
-                tiendas = JSON.parse(archivoTiendas);
-            }
+                Promise.all([pedidoProducto, pedidoTiendas])
+                    .then(function([producto, tiendas]){
+                        res.render('productEdit',{producto: producto, tiendas:tiendas});
 
-            nombreTiendas = [];
-            tiendas.forEach((tiend,i) => {
-                nombreTiendas.push(tiend.nombreTienda);
-            })
-            return res.render('productEdit',{producto: producto, tiendas:nombreTiendas});
+                    })
         },
 
+            //FALTA
+
         put_editProducto: function (req,res){
-            let archivoProductos=fs.readFileSync('data_productos.json', {encoding: 'utf-8'});
-            let productos;
-            if(archivoProductos == ""){
-                productos =[];
-            } else {
-                productos = JSON.parse(archivoProductos);
-            }
 
-            //console.log (req.body.descCorta);
-
-                productos.map((prod) => { // Modificamos el array de productos
-                     if (prod.codigo == req.body.codigo) {
-                            prod.codigo = req.body.codigo,
-                            prod.nombreProducto = req.body.nombreProduct,
-                            prod.descCorta = req.body.descCorta,
-                            prod.descLarga = req.body.descLarga,
-                            prod.precio = req.body.precio,
-                            prod.tienda = req.body.tienda,
-                            prod.filtroOrg = req.body.org,
-                            prod.filtroSinTacc = req.body.sinTacc,
-                            prod.filtrosSinLactosa = req.body.sinLactosa,
-                            prod.fotoProducto = req.files[0].filename
-                     }
-                  });
-
-                  productosJSON = JSON.stringify(productos);
-                  fs.writeFileSync('data_productos.json', productosJSON);
-
+            db.Producto.update({
+                name: req.body.nombreProduct,
+                short_description: req.body.descCorta,
+                long_description: req.body.descLarga,
+                price: req.body.price,
+                store_id: req.body.tienda,
+                image: req.files[0].filename,
+                filtroOrg = req.body.org,
+                filtroSinTacc = req.body.sinTacc,
+                filtrosSinLactosa = req.body.sinLactosa,
+            }, {
+                where: {
+                id: req.params.codigo
+                }
+            });
                   res.redirect("/productos");
-               },
+        },
 
         listadoTiendas: function (req,res){
-                let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-                let tiendas;
-                if(archivoTiendas == ""){
-                    tiendas =[];
-                } else {
-                    tiendas = JSON.parse(archivoTiendas);
-                }
+            db.Tienda.findAll()
+                .then(function(tiendas){
                     res.render('listadoTiendas', {tiendas: tiendas});
-            },
+                })
+        },
 
         get_searchTiendas: function (req,res){
-                let nombreBuscado =  req.query.nombre;
-
-                let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-                let tiendas;
-                if(archivoTiendas == ""){
-                    tiendas =[];
-                } else {
-                    tiendas = JSON.parse(archivoTiendas);
-                }
-                let results = [];
-                tiendas.forEach((tiend, i) => {
-                    if (tiend.nombreTienda.includes(nombreBuscado)) {
-                        results.push(tiend);
-                    }
-                });
-                return res.render('tiendaBuscada', {tiendas: results});
-            },
+            db.Tienda.findAll()
+                .then(function(tiendas){
+                    let nombreBuscado =  req.query.nombre;
+                    let results = [];
+                    tiendas.forEach((tiend, i) => {
+                        if (tiend.name.includes(nombreBuscado)) {
+                            results.push(tiend);
+                        }
+                    });
+                    return res.render('listadoTiendas', {tiendas: results});
+                })
+        },
 
         nuevaTienda: function (req,res){
                 res.render('tiendaAdd');
             },
 
         crearTienda: function(req, res){
-                console.log(req.files);
-
-                let tienda = {
-                    codigo: req.body.codigo,
-                    nombreTienda: req.body.nombreTienda,
-                    descLarga: req.body.descLarga,
-                    fotoTienda: req.files[0].filename
-                }
-
-                //GUARDAR INFORMACION DEL FORMULARIO EN UN ARCHIVO
-
-                //1ºLeer el archivo
-                let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-                let tiendas;
-                if(archivoTiendas == ""){
-                    tiendas =[];
-                } else {
-                    tiendas = JSON.parse(archivoTiendas);
-                }
-
-                //agregamos la nueva tienda
-                tiendas.push(tienda);
-                //Escribir el archivo
-                tiendasJSON = JSON.stringify(tiendas);
-
-                fs.writeFileSync('data_tiendas.json', tiendasJSON);
+            db.Tienda.create({
+                name: req.body.nombreTienda,
+                description: req.body.descLarga,
+                image: req.files[0].filename
+            })
 
                 res.redirect('/productos/tiendas');
             },
 
+
         get_editTienda: function(req,res){
-                let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-                let tiendas;
-                if(archivoTiendas == ""){
-                    tiendas =[];
-                } else {
-                    tiendas = JSON.parse(archivoTiendas);
-                }
 
-                let tienda = null;
-                tiendas.forEach((tiend, i) => {
-                    if (tiend["codigo"] == req.params.codigo) {
-                        tienda = tiend;
-                    }
-                });
-
-                return res.render('tiendaEdit', {tienda: tienda});
+            db.Tienda.findByPk(req.params.codigo)
+                .then(function(tienda){
+                    return res.render('tiendaEdit', {tienda: tienda});
+                })
             },
 
         put_editTienda: function (req,res){
 
-                let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-                let tiendas;
-                if(archivoTiendas == ""){
-                    tiendas =[];
-                } else {
-                    tiendas = JSON.parse(archivoTiendas);
+            db.Tienda.update({
+                name: req.body.nombreTienda,
+                description: req.body.descLarga,
+                image: req.files[0].filename
+            }, {
+                where: {
+                id: req.params.codigo
                 }
-
-                tiendas.map((tiend) => { // Modificamos el array de tiendas
-                         if (tiend.codigo == req.body.codigo) {
-                                tiend.codigo = req.body.codigo,
-                                tiend.nombreTienda = req.body.nombreTienda,
-                                tiend.descLarga = req.body.descLarga,
-                                tiend.fotoTienda = req.files[0].filename
-                         }
-                      });
-
-                      tiendasJSON = JSON.stringify(tiendas);
-                      fs.writeFileSync('data_tiendas.json', tiendasJSON);
-                      res.redirect("/productos/tiendas");
-                   },
+            })
+                .then(function(){
+                    res.redirect("/productos/tiendas");
+                })
+        },
 
         deleteTienda: function(req,res){
-                let archivoTiendas=fs.readFileSync('data_tiendas.json', {encoding: 'utf-8'});
-                let tiendas;
-                if(archivoTiendas == ""){
-                    tiendas =[];
-                } else {
-                    tiendas = JSON.parse(archivoTiendas);
-                }
-                let tienda = null;
-                tiendas.forEach((tiend, i) => {
-                    if (tiend["codigo"] == req.params.codigo) {
-                        tienda = tiend;
-                    }
-                });
-                let nuevoArrayTiendas = [];
-                nuevoArrayTiendas = tiendas.filter(tiend => tiend.codigo != tienda.codigo);
-                nuevoArrayTiendasJSON = JSON.stringify(nuevoArrayTiendas);
-                fs.writeFileSync('data_tiendas.json', nuevoArrayTiendasJSON);
-                return res.render("listadoTiendas",{tiendas: nuevoArrayTiendas});
-            },
 
+            db.Tienda.destroy({
+                where: {id: req.params.codigo}
+            })
+                .then(function(){
+                    res.redirect("/productos/tiendas");
+                })
+            },
     }
 
     module.exports = productosController;
