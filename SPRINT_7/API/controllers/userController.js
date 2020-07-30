1849477e23adf5aef56cbcc869d020fd424cda96
verosardin <vero.sardin28@gmail.com>
2020-07-30 20:55:52 -0300

    La API de usuarios quedó OK

--- SPRINT_7/API/controllers/userController.js
+++ SPRINT_7/API/controllers/userController.js
@@ -11,10 +11,11 @@ const userController = {
 //Acá falta eliminar password y category y agregar url de cada usuario.
          db.Usuario.findAll({attributes: ['id','first_name', 'last_name', 'email']})
             .then(function(usuarios){
-            for(let i = 0; i < usuarios.lengt; i++){
-                usuarios[i].setDataValue("detail", "api/users/" + usuarios[i].id)
+            for(let i = 0; i < usuarios.length; i++){
+                usuarios[i].setDataValue("detail", "http://localhost:3000/api/users/" + usuarios[i].id)
             }
-            res.status(200).json({estado: 'OK', item_category, item_count: usuarios.length, items: usuarios})
+
+            res.status(200).json({link: 'http://localhost:3000/api/users', estado: 'OK', item_category, item_count: usuarios.length, items: usuarios})
         })
         .catch(function(e){
             console.log(e)
@@ -24,7 +25,7 @@ const userController = {
     detalleUsuario: function(req,res) {
         db.Usuario.findByPk(req.params.id, {attributes: ['id','first_name', 'last_name', 'email']})
             .then(function(usuario){
-            res.status(200).json({estado: "OK", item_category, item_id: usuario.id, item: usuario})
+            res.status(200).json({link: 'http://localhost:3000/api/users', estado: "OK", item_category, item_id: usuario.id, item: usuario})
             })
     }
 }
